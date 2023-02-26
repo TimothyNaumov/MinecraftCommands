b3a4c6d49388b347fdbe8eed659a856b8ee1c2df
Tim <44584257+TimothyNaumov@users.noreply.github.com>
2023-02-25 18:06:55 -0600

    overrode onCommand method

--- src/main/java/me/timothynaumov/commands/Commands.java
+++ src/main/java/me/timothynaumov/commands/Commands.java
@@ -1,5 +1,8 @@
 package me.timothynaumov.commands;
 
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Commands extends JavaPlugin {
@@ -7,11 +10,10 @@ public final class Commands extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
     }
 
     @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
+        return true;
     }
 }
